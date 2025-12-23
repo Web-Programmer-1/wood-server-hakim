22081ef895e65a90b84e390c2bf8d83f45f67b9b
tecnology-expart <mdhakimshorkar123@gmail.com>
2025-12-23 20:30:00 +0600

    rejoin-fix

--- src/config/aws.config.ts
+++ src/config/aws.config.ts
@@ -1,11 +1,28 @@
 
+// import { S3Client } from "@aws-sdk/client-s3";
+
+// export const s3 = new S3Client({
+//   region: process.env.AWS_REGION || "us-east-1",
+//   credentials: {
+//     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+//   },
+// });
+
+
+
+
 import { S3Client } from "@aws-sdk/client-s3";
 
+if (!process.env.AWS_REGION) {
+  throw new Error("AWS_REGION is not defined");
+}
+
 export const s3 = new S3Client({
-  region: process.env.AWS_REGION || "us-east-1",
+  region: process.env.AWS_REGION, // ✅ eu-north-1
+  endpoint: `https://s3.${process.env.AWS_REGION}.amazonaws.com`, // ✅ IMPORTANT
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
 });
-
