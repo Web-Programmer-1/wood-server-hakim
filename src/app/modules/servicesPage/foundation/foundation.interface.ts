fe2157217831a5c6d23b2d5d0eaa83fec25cd9c7
tecnology-expart <mdhakimshorkar123@gmail.com>
2026-03-07 15:48:21 +0600

    latest-working

---
+++ src/app/modules/servicesPage/foundation/foundation.interface.ts
@@ -0,0 +1,24 @@
+export type ICreateFoundationStoryPayload = {
+  title: string;
+  slug: string;
+  description?: string;
+//   cardImage: string;
+  cardImageUrl: string;
+
+  videoType?: string;
+  youtubeUrl?: string;
+  videoUrl?: string;
+
+  galleryImages?: any;
+
+  sortOrder?: number;
+};
+
+
+
+
+export type IGetAllFoundationStoryQuery = {
+  search?: string;
+  page?: number;
+  limit?: number;
+};
