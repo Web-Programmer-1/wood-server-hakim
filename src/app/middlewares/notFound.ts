36edbb62e97918e3675537a3a8be9f7e02bf3359
tecnology-expart <mdhakimshorkar123@gmail.com>
2026-03-31 11:59:07 +0600

    31-3-26-lastest-push

--- src/app/middlewares/notFound.ts
+++ src/app/middlewares/notFound.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express"
 import httpStatus from "http-status"
-
+// not found middleware
 const notFound = (req: Request, res: Response, next: NextFunction) => {
     res.status(httpStatus.NOT_FOUND).json({
         success: false,
