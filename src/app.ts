0a857536afa9b92187b04adec7b4d3c984bc525c
Walid Hasan <walid.official8@gmail.com>
2026-04-26 12:40:49 +0600

    fix: limit

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,7 @@ const app: Application = express();
 app.set('trust proxy', 1);
 
 const allowedOrigins = [
+  "http://localhost:3000", 
   "https://woodtechsolutionbd.com",
   "https://www.woodtechsolutionbd.com"
 ];
@@ -47,9 +48,9 @@ app.use('/health', healthRoutes);
 app.use(globalLimiter);
 
 // parsers
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Cron (optional)
 if (process.env.ENABLE_DEBUG_CRON === 'true') {
